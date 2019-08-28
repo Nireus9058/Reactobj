dddb21898f24e7aa4bc1afb3c914f93b03b3a630
Nireus9058 <1959493406@qq.com>
2019-08-28 17:32:28 +0800

    react 第一次提交

---
+++ config/server.js
@@ -0,0 +1,15 @@
+module.exports={
+  local:{
+    open:false,
+    port:3000
+  },
+  http:{
+    open:true,
+    port:3001,
+  },
+  
+  https:{
+    open:false,
+    port:443
+  }
+}
---
+++ public/template/precache-manifest.862bf683540cf542978b2c97742f069d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbb71f138316aa83ac046b2ceb4b8a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5109594bfa128281f15",
+    "url": "/static/css/2.78344a8a.chunk.css"
+  },
+  {
+    "revision": "e69608d425dd83f8192b",
+    "url": "/static/css/main.86750d7f.chunk.css"
+  },
+  {
+    "revision": "a5109594bfa128281f15",
+    "url": "/static/js/2.77a33373.chunk.js"
+  },
+  {
+    "revision": "e69608d425dd83f8192b",
+    "url": "/static/js/main.9eaf9487.chunk.js"
+  },
+  {
+    "revision": "8771ea939ba2d19b6d44",
+    "url": "/static/js/runtime~main.156eb43b.js"
+  },
+  {
+    "revision": "1a8448faf7d699ee1089ece2903a59c5",
+    "url": "/static/media/font_1461139240_0312312.1a8448fa.ttf"
+  },
+  {
+    "revision": "70ace2c2341c2e17c24f322a5cd46193",
+    "url": "/static/media/iconfont.70ace2c2.ttf"
+  },
+  {
+    "revision": "76be2f3fa9fc6f234037f71ad906cfa7",
+    "url": "/static/media/iconfont.76be2f3f.svg"
+  },
+  {
+    "revision": "e67c5952bc14da69dc1fc8553e0a5b9a",
+    "url": "/static/media/iconfont.e67c5952.eot"
+  }
+]);
